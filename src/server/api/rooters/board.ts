7f15de59c1f024535394acfa69ecadb0e020f514
JBPORTALS <jbportalsblr@gmail.com>
2025-10-31 17:06:27 +0530

    feat(board): Introduce board and board_member schemas with validation; update API to include board router for enhanced project management

---
+++ src/server/api/rooters/board.ts
@@ -0,0 +1,42 @@
+import { z } from "zod/v4";
+import { protectedProcedure } from "../trpc";
+import {
+  board,
+  boardMember,
+  CreateBoardSchema,
+  UpdateBoardSchema,
+} from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+
+export const boardRouter = {
+  create: protectedProcedure
+    .input(CreateBoardSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.insert(board).values(input);
+    }),
+
+  update: protectedProcedure
+    .input(UpdateBoardSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db
+        .update(board)
+        .set(input)
+        .where(eq(board.id, input.id));
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.delete(board).where(eq(board.id, input.id));
+    }),
+  list: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.query.board.findMany({
+      // select({name:board.name}).from(board).where......
+      with: {
+        boardMembers: {
+          where: eq(boardMember.userId, ctx.session.user.id),
+        },
+      },
+    });
+  }),
+};
