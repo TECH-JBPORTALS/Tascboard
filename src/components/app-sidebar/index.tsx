60bee04b29e0604882d319f78775554fd19e93aa
x-sss-x <manu48617@gmail.com>
2025-11-11 13:23:31 +0530

    feat(sidebar): Refactor AppSidebar to include data prefetching and add loading skeleton for boards menu

--- src/components/app-sidebar/index.tsx
+++ src/components/app-sidebar/index.tsx
@@ -11,6 +11,7 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSkeleton,
 } from "@/components/ui/sidebar";
 import { NavMain } from "./nav-main";
 import { NavBoards } from "./nav-boards";
@@ -18,48 +19,67 @@ import { NavUser } from "./nav-user";
 import Image from "next/image";
 import Link from "next/link";
 import { PlusIcon } from "lucide-react";
+import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export async function AppSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  prefetch(trpc.board.list.queryOptions());
+
   return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              size="lg"
-              className="hover:cursor-auto hover:bg-transparent active:bg-transparent"
-              asChild
-            >
-              <Link href={"/"}>
-                <Image
-                  width={32}
-                  height={32}
-                  alt="Tascboard Logo"
-                  src={"/tascboard.svg"}
-                />
-                <div className="grid flex-1 text-left text-lg leading-tight">
-                  <span className="truncate font-sans font-semibold">
-                    Tascboard
-                  </span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-        <NavMain />
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Boards</SidebarGroupLabel>
-          <SidebarGroupAction>
-            <PlusIcon />
-          </SidebarGroupAction>
-          <NavBoards />
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser />
-      </SidebarFooter>
-    </Sidebar>
+    <HydrateClient>
+      <Sidebar collapsible="icon" {...props}>
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                size="lg"
+                className="hover:cursor-auto hover:bg-transparent active:bg-transparent"
+                asChild
+              >
+                <Link href={"/"}>
+                  <Image
+                    width={32}
+                    height={32}
+                    alt="Tascboard Logo"
+                    src={"/tascboard.svg"}
+                  />
+                  <div className="grid flex-1 text-left text-lg leading-tight">
+                    <span className="truncate font-sans font-semibold">
+                      Tascboard
+                    </span>
+                  </div>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+          <NavMain />
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Boards</SidebarGroupLabel>
+            <SidebarGroupAction>
+              <PlusIcon />
+            </SidebarGroupAction>
+            <React.Suspense fallback={<SidebarBoardsMenuSkeleton />}>
+              <NavBoards />
+            </React.Suspense>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <NavUser />
+        </SidebarFooter>
+      </Sidebar>
+    </HydrateClient>
+  );
+}
+
+function SidebarBoardsMenuSkeleton() {
+  return (
+    <SidebarMenu>
+      {Array.from({ length: 6 }).map((_, i) => (
+        <SidebarMenuSkeleton key={i} />
+      ))}
+    </SidebarMenu>
   );
 }
