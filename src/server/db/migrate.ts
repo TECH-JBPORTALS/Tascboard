fa69e8e85c5accfbdcb4a8efb492ee9e91f04823
x-sss-x <manu48617@gmail.com>
2025-12-22 21:26:38 +0530

    refactor(Database Migration Logic): Remove outdated logic for updating Tasc face IDs from migration script, streamlining the migration process and improving code clarity.

--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -1,32 +1,5 @@
 import { migrate } from "drizzle-orm/neon-http/migrator";
 import { db } from ".";
-import { tasc } from "./schema";
-import { eq } from "drizzle-orm";
-import { asc } from "drizzle-orm";
-
-console.log("Fixing tasc face Id's...");
-
-// Get all tracks
-await db.query.track.findMany().then(async (tracks) => {
-  return await Promise.all(
-    tracks.map(async (tr) => {
-      // Get all tascs in track in ascending order by createdAt
-      const tascs = await db.query.tasc.findMany({
-        where: eq(tasc.trackId, tr.id),
-        orderBy: asc(tasc.createdAt),
-      });
-
-      return await Promise.all(
-        tascs.map((ta, i) =>
-          db
-            .update(tasc)
-            .set({ faceId: (i + 1).toString() })
-            .where(eq(tasc.id, ta.id)),
-        ),
-      );
-    }),
-  );
-});
 
 console.log("Applying migrations....");
 
